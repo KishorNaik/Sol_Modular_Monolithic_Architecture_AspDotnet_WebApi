83c9cd79d923d981ad71f25833c163ec183b381d
Kishor Naik <kishor.naik011.net@gmail.com>
2024-12-17 11:19:54 +0530

    @KishorNaik: Added User Claims Provider

---
+++ Sol_Demo/User.Contracts/Shared/Services/UserClaimsProviders/Contract.cs
@@ -0,0 +1,10 @@
+﻿using Models.Shared.Enums;
+
+namespace User.Contracts.Shared.Services.UserClaimsProviders;
+
+public interface IUserClaimsProvider
+{
+    Guid GetUserIdentifier();
+
+    UserTypeEnum GetUserType();
+}
---
+++ Sol_Demo/User.Shared/Services/UserClaimsProvider/Activity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Models.Shared.Enums;
+using sorovi.DependencyInjection.AutoRegister;
+using System.Security.Claims;
+using User.Contracts.Shared.Services.UserClaimsProviders;
+
+namespace User.Shared.Services.UserClaimsProvider;
+
+[ScopedService(typeof(IUserClaimsProvider))]
+public class UserClaimsProvider : IUserClaimsProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserClaimsProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    Guid IUserClaimsProvider.GetUserIdentifier()
+    {
+        return Guid.Parse(_httpContextAccessor!.HttpContext!.User.Claims
+                        .First(i => i.Type == ClaimTypes.NameIdentifier).Value);
+    }
+
+    UserTypeEnum IUserClaimsProvider.GetUserType()
+    {
+        var userRole = _httpContextAccessor!.HttpContext!.User.Claims.First(i => i.Type == ClaimTypes.Role).Value;
+
+        UserTypeEnum userRoleEnum;
+        Enum.TryParse(userRole, out userRoleEnum);
+        return userRoleEnum;
+
+    }
+}
